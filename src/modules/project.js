ac386cd014b5e9d737526fea8a5472b30f1bca74
Ace <kronicrespawn@gmail.com>
2022-08-26 22:19:08 -0500

    add project functionality

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,17 +1,40 @@
-export const projects = (() => {    
+export const projects = (() => { 
+    let count = 1;
     const panel = document.querySelector('.sidepanel')
     const newProject = document.createElement('button');
     const projectName = document.createElement('p');
     const section = document.createElement('div');
-    section.className = 'block';
+    section.className = 'pblock';
     projectName.className= 'project_name';
-    projectName.textContent= 'Project';
+    projectName.textContent= 'Create Project';
     newProject.className= 'addProject';
     newProject.textContent='+';
     panel.appendChild(section);
     section.appendChild(newProject); 
     section.appendChild(projectName);   
 
-    // return { };
+    const projectAdd = () => {
+        const panel = document.querySelector('.sidepanel')
+        const editProject = document.createElement('button');
+        const projectName = document.createElement('p');
+        const deleteProject = document.createElement('button');
+        const section = document.createElement('div');
+        section.className = 'pblock';
+        projectName.className = 'projectList';
+        projectName.textContent = 'Project ' + `${count}`;
+        deleteProject.className = 'removeProject';
+        deleteProject.textContent = 'X';
+        editProject.className= 'editProject';
+        editProject.textContent='edit';
+        panel.appendChild(section);
+        section.appendChild(deleteProject); 
+        section.appendChild(editProject);
+        section.appendChild(projectName);
+        count ++
+    }
+
+    newProject.addEventListener('click', projectAdd);
+
+    return { projectAdd };
     })
 ();
