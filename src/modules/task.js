047a85c20153c36eeeeb84ddb812058c48bca3be
Ace <kronicrespawn@gmail.com>
2022-08-26 22:19:38 -0500

    add task functionality

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,3 +1,4 @@
+
 export const task = (() => {    
     let count = 1;
     const $content = document.querySelector('.content')
@@ -6,10 +7,9 @@ export const task = (() => {
     const sectionTask = document.createElement('div');
     const taskList = document.createElement('div');
     taskList.className = 'taskList';
-    taskList.textContent = 'Task ' + `${count}`;
+    taskList.textContent = 'Create Task';
     sectionTask.className = 'block';
-    taskName.className= 'project_name';
-    taskName.textContent= 'Project';
+    taskName.className= 'taskName';
     newTask.className= 'addProject';
     newTask.textContent='+';
     $content.appendChild(sectionTask);
@@ -17,6 +17,41 @@ export const task = (() => {
     sectionTask.appendChild(taskList);
     sectionTask.appendChild(taskName);   
 
-    // return { };
+    const taskAdd = () => {
+    const $content = document.querySelector('.content')
+    const editTask = document.createElement('button');
+    const deleteTask = document.createElement('button');
+    const taskInput = document.createElement('p');
+    const sectionTask = document.createElement('div');
+    const taskList = document.createElement('div');
+    const pickDate = document.createElement('input');
+    pickDate.setAttribute('type', 'date');
+    taskList.className = 'taskList';
+    taskList.textContent = 'Task ' + `${count}`;
+    deleteTask.className = 'deleteTask';
+    deleteTask.textContent = 'X';
+    sectionTask.className = 'block';
+    taskInput.className= 'taskInput';
+    taskInput.textContent= taskName.value;
+    editTask.className= 'editTask';
+    editTask.textContent='edit';
+    $content.appendChild(sectionTask);
+    sectionTask.appendChild(editTask);
+    sectionTask.appendChild(deleteTask); 
+    sectionTask.appendChild(taskList);
+    sectionTask.appendChild(taskInput); 
+    sectionTask.appendChild(pickDate);
+    count ++
+    taskName.value = '';
+    }
+
+    // const removeTask = () => {
+    //     this.removeChild(this.firstChild);
+    // }
+
+    // deleteTask.addEventListener('click', removeTask)
+    newTask.addEventListener('click', taskAdd);
+
+    return { taskAdd};
     })
 ();
