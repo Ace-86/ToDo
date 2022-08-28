b812cc0fb3b39cce6b39f9149700eeff7036901d
Ace <kronicrespawn@gmail.com>
2022-08-27 22:12:51 -0500

    moved dom elements to another module

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,40 +1,9 @@
+import { Dom } from './dom.js';
+
+
 export const projects = (() => { 
-    let count = 1;
-    const panel = document.querySelector('.sidepanel')
-    const newProject = document.createElement('button');
-    const projectName = document.createElement('p');
-    const section = document.createElement('div');
-    section.className = 'pblock';
-    projectName.className= 'project_name';
-    projectName.textContent= 'Create Project';
-    newProject.className= 'addProject';
-    newProject.textContent='+';
-    panel.appendChild(section);
-    section.appendChild(newProject); 
-    section.appendChild(projectName);   
-
-    const projectAdd = () => {
-        const panel = document.querySelector('.sidepanel')
-        const editProject = document.createElement('button');
-        const projectName = document.createElement('p');
-        const deleteProject = document.createElement('button');
-        const section = document.createElement('div');
-        section.className = 'pblock';
-        projectName.className = 'projectList';
-        projectName.textContent = 'Project ' + `${count}`;
-        deleteProject.className = 'removeProject';
-        deleteProject.textContent = 'X';
-        editProject.className= 'editProject';
-        editProject.textContent='edit';
-        panel.appendChild(section);
-        section.appendChild(deleteProject); 
-        section.appendChild(editProject);
-        section.appendChild(projectName);
-        count ++
-    }
-
-    newProject.addEventListener('click', projectAdd);
-
-    return { projectAdd };
+    Dom.projects();
+    const newProject = document.querySelector('button');
+    newProject.addEventListener('click', Dom.projectAdd);
     })
 ();
