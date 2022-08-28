82c9890c9d0aff251c1079abd6117da90341f8a6
Ace <kronicrespawn@gmail.com>
2022-08-27 22:15:24 -0500

    removed old code

--- src/index.js
+++ src/index.js
@@ -3,16 +3,3 @@ import './style.css';
 import { projects } from './modules/project.js';
 import { task } from './modules/task.js';
 
-// const projects = (() => {    
-//     const panel = document.querySelector('.sidepanel')
-//     const newProject = document.createElement('button');
-//     newProject.className= 'addProject';
-//     panel.appendChild(newProject);    
-
-   
-    
-    
-
-//     // return { };
-//     })
-// ();
