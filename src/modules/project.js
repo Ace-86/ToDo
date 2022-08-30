0990d6e9ef392f4c88fdf531fe3b56564796aabd
Ace <kronicrespawn@gmail.com>
2022-08-29 20:54:40 -0500

    remove project button (update)

--- src/modules/project.js
+++ src/modules/project.js
@@ -4,6 +4,14 @@ import { Dom } from './dom.js';
 export const projects = (() => { 
     Dom.projects();
     const newProject = document.querySelector('button');
+    const panel = document.querySelector('.sidepanel');
+    const removeProjec = document.querySelector('.removeProject');
+    // const blockData  = document.querySelector('.pBlock');
+   
+    
     newProject.addEventListener('click', Dom.projectAdd);
+    
+
+
     })
 ();
