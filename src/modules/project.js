d8214550eb855e8bdddc16b22d41e4630a343874
Ace <kronicrespawn@gmail.com>
2022-08-30 09:48:46 -0500

    removed code

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,11 +1,12 @@
 import { Dom } from './dom.js';
+// import { modals } from '.modal.js';
 
 
 export const projects = (() => { 
     Dom.projects();
-    const newProject = document.querySelector('.addProject');
-    const panel = document.querySelector('.sidepanel');
-    const removeProjec = document.querySelector('.removeProject');
+    const newProject = document.querySelector('#submit1');
+    // const panel = document.querySelector('.sidepanel');
+    // const removeProjec = document.querySelector('.removeProject');
     newProject.addEventListener('click', Dom.projectAdd);
     })
 ();
