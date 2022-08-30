a617454466464a63162cfeb218a547d39333cece
Ace <kronicrespawn@gmail.com>
2022-08-30 09:48:17 -0500

    project modal submit button functional

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,6 +4,7 @@ export const modals = (() => {
     const startModal = document.querySelector('.projectModal');
     const $modalForm = document.querySelector('#modalForm');
     const $exitModal = document.querySelector('.exit');
+    const $submit = document.querySelector('#submit1');
    //add button (modal popup)
     startModal.onclick = function() {
         $modalForm.style.display = "block";
@@ -14,18 +15,17 @@ export const modals = (() => {
     $exitModal.onclick = function () {
         $modalForm.style.display = "none";
     };
-    //modal submit button
-    // $submit.onclick = function() {
-    //     getUserInput();
-    //     createCard();
-    //     $modalForm.style.display = "none";
-    // };
     
-    // closes modal when clicked outside modal window
+    
     window.onclick = function(event) {
         if (event.target == $modalForm) {
             $modalForm.style.display = "none";
         }
     };
+
+    $submit.addEventListener('click', function() {
+        $modalForm.style.display = 'none';
+    });
+
 })
 ()
