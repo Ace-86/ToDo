4539b2cb49ca9a8e301eb383622b7d3a8eabafe1
Ace <kronicrespawn@gmail.com>
2022-08-30 20:57:01 -0500

    removed task module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import _ from 'lodash';
 import './style.css';
 import { projects } from './modules/project.js';
-import { task } from './modules/task.js';
 import { modals } from './modules/modal.js';
+// import { task } from './modules/task.js';
