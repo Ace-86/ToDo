284d145a0c3ce34f0abf1c537c2ee53a57dc0864
Ace <kronicrespawn@gmail.com>
2022-09-07 22:11:09 -0500

    file emptied

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,8 +1,8 @@
-// import { Dom } from './dom.js';
 
-// export const task = (() => { 
-//     // Dom.task();
-//     const newTask = document.querySelector('.addTask');
-//     newTask.addEventListener('click', Dom.taskAdd);
-//     })
-// ();
+
+export const $task = (() => {
+
+
+
+})
+();
