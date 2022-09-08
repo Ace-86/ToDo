2c1fb9ee7bfc5a094ca1cab09d8eb04fbbdb791b
Ace <kronicrespawn@gmail.com>
2022-09-07 22:10:10 -0500

    dummy content remmoved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 // import _ from 'lodash';
 import './style.css';
 import { script } from './modules/script.js';
-// import { modals } from './modules/modal.js';
-// import { task } from './modules/task.js';
