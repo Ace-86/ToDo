65cf7191531bd50a52499bbe30c68b1ce2a8deaa
Ace <kronicrespawn@gmail.com>
2022-09-10 23:26:00 -0500

    added event; not functioning correctly

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,6 +1,15 @@
+import { $storage } from './storage.js';
+import { script } from './script.js';
 
 export const $listener = (() => {
+const listsContainer = document.querySelector('[data-lists]')
 
+    listsContainer.addEventListener('click', e => {
+        if (e.target.tagName.toLowerCase() === 'li') {
+            selectedListId = e.target.dataset.listId
+            saveAndRender()
+        }
+    })
 
 
  })
