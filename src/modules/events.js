8072f143f6c03f8de1008c01daef7740787da0dc
Ace <kronicrespawn@gmail.com>
2022-09-12 22:03:51 -0500

    comment out code

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,16 +1,16 @@
-import { $storage } from './storage.js';
-import { script } from './script.js';
+// import { $storage } from './storage.js';
+// import { script } from './script.js';
 
-export const $listener = (() => {
-const listsContainer = document.querySelector('[data-lists]')
+// export const $listener = (() => {
+// const listsContainer = document.querySelector('[data-lists]')
 
-    listsContainer.addEventListener('click', e => {
-        if (e.target.tagName.toLowerCase() === 'li') {
-            selectedListId = e.target.dataset.listId
-            saveAndRender()
-        }
-    })
+//     listsContainer.addEventListener('click', e => {
+//         if (e.target.tagName.toLowerCase() === 'li') {
+//             selectedListId = e.target.dataset.listId
+//             saveAndRender()
+//         }
+//     })
 
 
- })
-();
+//  })
+// ();
