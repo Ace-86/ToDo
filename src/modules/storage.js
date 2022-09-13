12da85856ff2b98f18e76948b1fffc509a0d3953
Ace <kronicrespawn@gmail.com>
2022-09-12 22:31:53 -0500

    comment out code

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,16 +1,16 @@
-import { script } from './script.js';
+// import { script } from './script.js';
 
-export const $storage = (() => {
-    const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-    const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId' 
-    let listsArr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-    let selectedListId = localStorage.getItem
-    (LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+// export const $storage = (() => {
+//     const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+//     const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId' 
+//     let listsArr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+//     let selectedListId = localStorage.getItem
+//     (LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
-    function save() {
-        localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(listsArr))
-        localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
-    }
-    return {save}
-})
-();
+//     function save() {
+//         localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(listsArr))
+//         localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+//     }
+//     return {save}
+// })
+// ();
